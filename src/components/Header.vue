5f4d40b7130a634b66b2ed4748778b4388a853b9
Ethan <ecpetta@gmail.com>
2021-12-02 20:59:28 -0700

    15.1 12/2 add page components and routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,39 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <router-link class="navbar-brand" to="/">Petta's Reviews</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <router-link class="nav-link active" aria-current="page" to="/movies">Movies</router-link>
+            </li>
+            
+            <li class="nav-item">
+                <router-link class="nav-link active" aria-current="page" to="/account">My Account</router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link class="nav-link active" aria-current="page" to="/login"><button class="btn btn-warning">Login</button></router-link>
+            </li>
+
+            <li class="nav-item">
+                <router-link class="nav-link active" aria-current="page" to="/signup"><button class="btn btn-outline-warning text-dark">Sign Up</button></router-link>
+            </li>
+
+            <li class="nav-item">
+                <button class="btn btn-warning">Logout</button>
+            </li>
+        </ul>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
+
+<style scoped>
+.navbar-nav{
+    align-items: center;
+}
+</style>
