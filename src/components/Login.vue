48814b3e9f97e1a93cc773be2e838424ed1fe164
Ethan <ecpetta@gmail.com>
2021-12-07 20:17:40 -0700

    13.2 12/7 axios, signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
       <h1>Login</h1>
+      <div v-if="this.$route.query.signupsuccess"
+      class="alert alert-success">
+        Thanks for signing up, please log in now
+        </div>
       </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from "axios";
 
 import myRouter from "./routes.js";
 
+axios.defaults.baseURL = "https://cis410-store-api.azurewebsites.net";
+
 const myApp = createApp(App)
 myApp.use(myRouter);
-myApp.mount('#app');
+myApp.mount("#app");
