d398c5a6a888def44dba4d69433f7f908e95f545
Ethan <ecpetta@gmail.com>
2021-12-14 22:01:44 -0700

    15.2 12/14 /product/:productID

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-    <h1>Product Detail</h1>
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-primary">{{ product.productName }}</h2>
+        <br />
+        <p>
+          Category: <br /><strong>{{ product.CategoryName }}</strong>
+        </p>
+        <p>
+          Price: <br /><strong>{{ formattedPrice }}</strong>
+        </p>
+      </div>
+    </div>
+    <br />
+
+    <router-link
+      v-if="auth"
+      :to="`/products/${this.$route.params.ProductID}/review`"
+      ><button class="btn btn-success">Add a Review</button></router-link
+    >
+
+    <router-link v-else :to="`/login`"
+      ><button class="btn btn-outline-success">
+        Sign In to Add a Review
+      </button></router-link
+    >
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    product() {
+      let allProducts = this.$store.state.products;
+
+      let thisProduct = allProducts.find((aProduct) => {
+        return aProduct.ProductID == this.$route.params.ProductID;
+      });
+
+      return thisProduct;
+    },
+    formattedPrice() {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(this.product.productPrice);
+    },
+    auth() {
+      return this.$store.state.token;
+    },
+  },
+};
 </script>
 
 <style>
